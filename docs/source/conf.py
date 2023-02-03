8d26fe69c1a8c69c34d29f849e19f919b8e6f2d6
Hui Wang <58657301+wanghui88888888@users.noreply.github.com>
2023-02-03 14:07:32 +0800

    1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Computer_Architecture_Notebook'
-copyright = '2022, kkqq'
-author = 'kkqq'
+project = 'A Roadmap for BioAI'
+copyright = '2023, wanghui'
+author = 'wanghui'
 
 release = '1.0'
 version = '1.0.0'
